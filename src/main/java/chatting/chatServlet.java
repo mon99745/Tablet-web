3f6fb2de06c344ea1e2751c6ffcbd3b82e9294dd
Jiwon <mon99745@gmail.com>
2021-08-25 18:18:02 +0900

    채팅 구현

---
+++ src/main/java/chatting/chatServlet.java
@@ -0,0 +1,69 @@
+package chatting;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/chatServlet")
+
+public class chatServlet  extends javax.servlet.http.HttpServlet implements javax.servlet.Servlet{
+	 static final long serialVersionUID = 1L;
+	 String message[];
+	 int index = 0, size = 10;
+	 
+	 public void init(){
+	  message = new String[size];
+	  for(int i=0; i<size; i++){
+	   message[i] = "";
+	  }
+	 }
+	 protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	  request.setCharacterEncoding("UTF-8");
+	  String id = request.getParameter("id");
+	  String msg = request.getParameter("msg");
+	  if(msg != null && msg.trim().length()!=0){
+	   synchronized(message){
+	    message[index] = id + " : " + msg;
+	    index = (index+1) % size;
+	   }
+	  }
+	  doGet(request, response);
+	 }
+	 
+	 public void doGet(HttpServletRequest request, HttpServletResponse response)throws IOException, ServletException{
+	  response.setContentType("text/html;charset=UTF-8");
+	  String first = request.getParameter("first");
+	  if(first != null){
+	   String id = request.getParameter("id");
+//	   id = myclasses.StringUtil.toHangul(id);
+	   synchronized(message){
+	    message[index] = "<p align=center>" + id + "님이 들어왔습니다. </p>";
+	    index = (index + 1) % size;
+	   }
+	  }
+	  PrintWriter out = response.getWriter();
+	  out.println("<html><head>");
+	  out.println("<meta http-equiv='pragma' content='no-cache'>");
+	  out.println("<meta http-equiv='cache-control' content='no-cache'>");
+	  out.println("<meta http-equiv='refresh'");
+	  out.println("content='2; URL=/chatting/chatServlet'>");
+	  out.println("<title>채팅</title></head>");
+	  out.println("<body><center><h2>채팅</h2></center><hr>");
+	  int i = index;
+	  while(true){
+	   out.print(message[i]);
+	   out.println("<br>");
+	   i = ++i % size;
+	   if(i==index-1)
+	    break;
+	   if(index == 0 && i == size-1)
+	    break;
+	  }
+	  out.print(message[i]);
+	  out.println("<br>");
+	  out.println("</body></html>");
+	 }
+	}
