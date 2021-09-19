ac9af253f695b5d16a04e13875b289b6b6d06164
Jiwon <mon99745@gmail.com>
2021-09-19 20:52:32 +0900

    Modal functional part -setting

--- src/main/java/com/cart/action/CartServlet.java
+++ src/main/java/com/cart/action/CartServlet.java
@@ -15,48 +15,62 @@ public class CartServlet extends HttpServlet{
 	
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("utf-8");
-		String name = request.getParameter("name");
+		doGet(request,response);
+	}
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		request.setCharacterEncoding("utf-8"); //한글처리 
+		
+		String name = request.getParameter("product");
 		int price = Integer.parseInt(request.getParameter("price"));
 		
-		//세션에서 상품 리스트를 받아옴 //세션생성
+		//세션 객체 생성
 		HttpSession session = request.getSession(true);
-		ArrayList<Products> gList = (ArrayList<Products>)session.getAttribute("list");
+		session.setAttribute("products",name); //세션에 정보 저장 
+		session.setAttribute("prices",price);
+		
+		
+		// 총합과 효율적인 호출을 위해 Product List 생성 
+		ArrayList<Products>	 pList = (ArrayList<Products>)session.getAttribute("list");
 		
 		//리스트가 없을경우 리스트를 생성
-		if(gList == null) gList = new ArrayList<Products>();
+		if(pList == null) pList = new ArrayList<Products>();
 		
 		//추가된 상품 객체를 리스트에 담음
-		gList.add(new Products(name,price));
+		pList.add(new Products(name,price));
 		
 		//세션에 리스트를 추가
-		session.setAttribute("list", gList);
+		session.setAttribute("list", pList);
 		
+		//세션의 정보를 리다이렉트 응답
+		response.sendRedirect("index.jsp");
 		
-		//페이지 출력
-		response.setContentType("text/html;charset=utf-8");
-		PrintWriter out = response.getWriter();
-		
-		out.println("<html><body><script>\n"
-//				+ "    alert('상품이 장바구니에 추가 되었습니다.');\n"
-				+ "	location.href = '/Danta/index.jsp';\n"
-				+ "</script>");
-		
-		
-		out.println("<p/><table width='80%'>");
-		out.println("<br/>[<a href='./index.jsp'>계속 쇼핑</a>]");
-		out.println("[<a href='Buy'>결제하기</a>]<br/>");
-		out.println("<tr><th>상품명</th><th>가격</th></tr>");
-		
-		//세션에 담긴 상품리스트를 반복문을 이용해 출력
-		for (int i = 0; i < gList.size(); i++) {
-			Products goods = (Products)gList.get(i);
-			out.println("<tr><td>" + goods.getName() + "</td>");
-			out.println("<td>" + goods.getPrice() + "</td></tr>");
-		}
-		out.println("</table>");
-		out.println("</body></html>");
-		out.close();
+//		
+//		//페이지 출력
+//		response.setContentType("text/html;charset=utf-8");
+//		PrintWriter out = response.getWriter();
+//		
+//		out.println("<html><body><script>\n"
+////				+ "    alert('상품이 장바구니에 추가 되었습니다.');\n"
+//				+ "	location.href = '/Danta/index.jsp';\n"
+//				+ "</script>");
+//		
+//		
+//		out.println("<p/><table width='80%'>");
+//		out.println("<br/>[<a href='./index.jsp'>계속 쇼핑</a>]");
+//		out.println("[<a href='Buy'>결제하기</a>]<br/>");
+//		out.println("<tr><th>상품명</th><th>가격</th></tr>");
+//		
+//		//세션에 담긴 상품리스트를 반복문을 이용해 출력
+//		for (int i = 0; i < gList.size(); i++) {
+//			Products goods = (Products)gList.get(i);
+//			out.println("<tr><td>" + goods.getName() + "</td>");
+//			out.println("<td>" + goods.getPrice() + "</td></tr>");
+//		}
+//		out.println("</table>");
+//		out.println("</body></html>");
+//		out.close();
 	}
 
 }
